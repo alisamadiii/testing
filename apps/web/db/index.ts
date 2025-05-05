154f3694f753680466bd7e5b5177d5342c8aca64
Ali Samadi <alirs.dev@gmail.com>
2025-05-04 21:13:50 -0700

    adsf

--- apps/web/db/index.ts
+++ apps/web/db/index.ts
@@ -1,5 +1,11 @@
-import "dotenv/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as dotenv from "dotenv";
+import * as schema from "./schema";
 
-import { drizzle } from "drizzle-orm/neon-http";
+dotenv.config();
 
-export const db = drizzle(process.env.DATABASE_URL as string);
+const connectionString = process.env.DATABASE_URL || "";
+const client = postgres(connectionString);
+
+export const db = drizzle(client, { schema });
