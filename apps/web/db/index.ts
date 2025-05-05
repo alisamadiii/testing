e32dea73dbc8c08c3bfcf6891792a819f07c3ea2
Ali Samadi <alirs.dev@gmail.com>
2025-05-04 20:03:25 -0700

    test 6

--- apps/web/db/index.ts
+++ apps/web/db/index.ts
@@ -2,6 +2,4 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/neon-http";
 
-export const db = drizzle(
-  "postgresql://neondb_owner:npg_l6V2mwrJhbAM@ep-aged-dust-a670n4ov-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require" as string
-);
+export const db = drizzle(process.env.DATABASE_URL as string);
